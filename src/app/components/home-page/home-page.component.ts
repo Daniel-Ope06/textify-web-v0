c7395dc5e3920cb4ce1c2e0f14e838a65613c55a
Daniel-Ope22 <97707320+Daniel-Ope06@users.noreply.github.com>
2023-01-23 07:46:36 +0100

    initialize

---
+++ src/app/components/home-page/home-page.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-page',
+  templateUrl: './home-page.component.html',
+  styleUrls: ['./home-page.component.scss']
+})
+export class HomePageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  submit() {
+    console.log("SUBMIT");
+    // Perform the submit action, such as sending data to a server
+  }
+
+}
