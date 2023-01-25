785556e98e43dbc8bded0b6feac312c2fd186e13
Daniel-Ope22 <97707320+Daniel-Ope06@users.noreply.github.com>
2023-01-25 21:04:45 +0100

    create `Number0`

---
+++ src/app/classes/numbers/number0.spec.ts
@@ -0,0 +1,7 @@
+import { Number0 } from './number0';
+
+describe('Number0', () => {
+  it('should create an instance', () => {
+    expect(new Number0()).toBeTruthy();
+  });
+});
---
+++ src/app/classes/numbers/number0.ts
@@ -0,0 +1,27 @@
+import { CharacterInterface } from "src/app/interfaces/character-interface";
+
+export class Number0 implements CharacterInterface {
+    oooo: string = "    ";
+    llll: string = "$$$$";
+
+    constructor(symbol: string){
+        this.llll = symbol;
+    }
+
+    getLine(lineNumber: number): string{
+        let line: string = "";
+        switch(lineNumber){
+            case 0: line = this.oooo + this.llll + this.oooo + this.oooo; break;
+            case 1: line = this.oooo + this.llll + this.oooo + this.oooo; break;
+            case 2: line = this.llll + this.oooo + this.llll + this.oooo; break;
+            case 3: line = this.llll + this.oooo + this.llll + this.oooo; break;
+            case 4: line = this.llll + this.oooo + this.llll + this.oooo; break;
+            case 5: line = this.llll + this.oooo + this.llll + this.oooo; break;
+            case 6: line = this.llll + this.oooo + this.llll + this.oooo; break;
+            case 7: line = this.llll + this.oooo + this.llll + this.oooo; break;
+            case 8: line = this.oooo + this.llll + this.oooo + this.oooo; break;
+            case 9: line = this.oooo + this.llll + this.oooo + this.oooo; break;
+        }
+        return line;
+    }
+}
