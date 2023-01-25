1a41bad53f0359b175c6ffc15f4d623267a47cc9
Daniel-Ope22 <97707320+Daniel-Ope06@users.noreply.github.com>
2023-01-25 10:01:16 +0100

    create `LetterG`

---
+++ src/app/classes/letters/letter-g.ts
@@ -0,0 +1,27 @@
+import { CharacterInterface } from "src/app/interfaces/character-interface";
+
+export class LetterG implements CharacterInterface {
+    oooo: string = "    ";
+    llll: string = "$$$$";
+
+    constructor(symbol: string){
+        this.llll = symbol;
+    }
+
+    getLine(lineNumber: number): string{
+        let line: string = "";
+        switch(lineNumber){
+            case 0: line = this.llll + this.llll + this.llll + this.oooo; break;
+            case 1: line = this.llll + this.llll + this.llll + this.oooo; break;
+            case 2: line = this.llll + this.oooo + this.oooo + this.oooo; break;
+            case 3: line = this.llll + this.oooo + this.oooo + this.oooo; break;
+            case 4: line = this.llll + this.oooo + this.llll + this.oooo; break;
+            case 5: line = this.llll + this.oooo + this.llll + this.oooo; break;
+            case 6: line = this.llll + this.oooo + this.llll + this.oooo; break;
+            case 7: line = this.llll + this.oooo + this.llll + this.oooo; break;
+            case 8: line = this.llll + this.llll + this.llll + this.oooo; break;
+            case 9: line = this.llll + this.llll + this.llll + this.oooo; break;
+        }
+        return line;
+    }
+}
