aaeede27a537847b0f6906a22e3ef584079bb5a1
Daniel-Ope22 <97707320+Daniel-Ope06@users.noreply.github.com>
2023-01-26 01:07:52 +0100

    create `Space`

---
+++ src/app/classes/special/space.ts
@@ -0,0 +1,27 @@
+import { CharacterInterface } from "src/app/interfaces/character-interface";
+
+export class Space implements CharacterInterface {
+    oooo: string = "    ";
+    llll: string = "$$$$";
+
+    constructor(symbol: string){
+        this.llll = symbol;
+    }
+
+    getLine(lineNumber: number): string{
+        let line: string = "";
+        switch(lineNumber){
+            case 0: line = this.oooo + this.oooo; break;
+            case 1: line = this.oooo + this.oooo; break;
+            case 2: line = this.oooo + this.oooo; break;
+            case 3: line = this.oooo + this.oooo; break;
+            case 4: line = this.oooo + this.oooo; break;
+            case 5: line = this.oooo + this.oooo; break;
+            case 6: line = this.oooo + this.oooo; break;
+            case 7: line = this.oooo + this.oooo; break;
+            case 8: line = this.oooo + this.oooo; break;
+            case 9: line = this.oooo + this.oooo; break;
+        }
+        return line;
+    }
+}
