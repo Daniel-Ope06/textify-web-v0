ce7131c3ed97fc863da19f80405b134bd5cca700
Daniel-Ope22 <97707320+Daniel-Ope06@users.noreply.github.com>
2023-05-21 22:41:46 +0100

    Add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { BirthdayGiftComponent } from './components/birthday-gift/birthday-gift.component';
+
+const routes: Routes = [
+  {path:'home', title: 'Textify', component: HomePageComponent},
+  {path:'emmanuella', title: 'Birthday - Textify', component: BirthdayGiftComponent},
+  {path:'', redirectTo: 'home', pathMatch: 'full'}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {scrollPositionRestoration: 'enabled'})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
