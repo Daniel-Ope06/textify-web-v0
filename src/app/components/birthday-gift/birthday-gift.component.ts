9fe31d336c3115aff1175273f34ede781f1cb18f
Daniel-Ope22 <97707320+Daniel-Ope06@users.noreply.github.com>
2023-05-21 22:39:49 +0100

    Create birthday-gift component

---
+++ src/app/components/birthday-gift/birthday-gift.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-birthday-gift',
+  templateUrl: './birthday-gift.component.html',
+  styleUrls: ['./birthday-gift.component.scss']
+})
+export class BirthdayGiftComponent {
+
+}
