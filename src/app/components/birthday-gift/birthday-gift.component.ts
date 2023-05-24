be1e1cae6e8d12a0b417b3a606eb25eb0a501f28
Daniel-Ope22 <97707320+Daniel-Ope06@users.noreply.github.com>
2023-05-24 02:40:27 +0100

    Completed set up of Ella's Birthday card

--- src/app/components/birthday-gift/birthday-gift.component.ts
+++ src/app/components/birthday-gift/birthday-gift.component.ts
@@ -7,14 +7,20 @@ import { Component } from '@angular/core';
 })
 export class BirthdayGiftComponent {
 
-  introduction: string = 'Happy Birthday';
-  greeting: string = 'Dear Emmanuella,';
-  fullName: string = 'Olanrewaju-Buoro Emmanuella';
-  birthdayLetter: string = "As I sit down to pen this letter, my heart is overflowing with emotions that words alone cannot fully express. I find myself falling deeper and deeper in love with you with each passing day, and I couldn't let another moment go by without letting you know just how special you are to me. From the very first time our eyes met, I felt an indescribable connection. It was as if the universe conspired to bring us together, weaving a love story destined to be extraordinary. Your presence in my life has illuminated my world, casting away any shadows of doubt or loneliness. With you, I feel complete. There is something magical about the way you make me feel. Your smile, like a sunbeam on a cloudy day, has the power to brighten even the darkest corners of my soul. Your laughter is a symphony that plays on repeat in my mind, filling my heart with joy. And your touch... oh, your touch sets my entire being ablaze, igniting a fire within me that burns with an intensity I've never known before. But it's not just your external beauty that captivates me; it's the beauty that lies within your soul. Your kindness, compassion, and unwavering support have taught me the true meaning of love. You've shown me that love isn't just a word, but a verb—a series of actions and choices we make every day to show our devotion and commitment. And I promise you, my love, that I will always choose you. You are my confidant, my best friend, and my partner in crime. Together, we have shared countless moments of laughter, tears, and everything in between. We've weathered storms and celebrated triumphs, always side by side, holding each other's hands. And in those moments, I've realized that with you, I am home. As we continue to embark on this journey together, I want you to know that I am committed to building a future filled with love, trust, and unwavering support. I promise to stand by your side through thick and thin, to be your rock when you need strength, and to lift you up when life throws its challenges our way. You have brought a depth of love and happiness into my life that I never thought possible. I am eternally grateful for your presence, and I cherish every moment we share together. You are the missing piece that completes me, the love that fills the emptiness in my heart. Without you, I am incomplete. So, my love, I hope these words convey even a fraction of the love that fills my heart. Know that you are cherished, adored, and loved beyond measure. You are my everything, and I am forever grateful to have you in my life.";
+  introHeading: string = 'Happy Birthday';
+  greeting: string = 'Dear Ella,';
+  introParagraph: string = "Wishing you the best this yearYou're a rare treasure, a rose among thorns. Here are words that remind me of you:";
+  introParagraphLines: string[] = this.splitIntoLines(this.introParagraph);
+
+  birthdayLetter: string = "Loving Honest Kind Thoughtful Elegant Beautiful Gracious Roses Pink Delicate Serene Radiant Charming Graceful Enchanting Brilliant Compassionate Authentic Glowing Charismatic Leader Refined Adorable Cute Aww Captivating Gentle Classy Exquisite Inspiring Angelic Violet Glittering Grateful Lavender Rare Peaceful Gleaming Generous Dazzling Precious Trustworthy Blissful Sparkling Seraphic Tender Calm Genuine Open Splendid Sincere Majestic Ethereal Mesmerizing Serene Steadfast Pearl Harmonious Opulent Delightful Timeless Joyful Shimmering Vibrant Gorgeous Radiating Superb Exotic Love Alluring Stunning Luminous Harmonious Magnificent Breathtaking Admired Thankful Mesmerizing Spellbinding Wonderful Irresistible Inspiring Exquisite Cherished Endearing Priceless Unique Treasured";
   birthdayLetterLines: string[] = this.splitIntoLines(this.birthdayLetter);
 
-  whiteColor: string = '#FBFFFE'//'#2495D9';
-  blackColor: string = '#242626';
+  fullName: string = 'Olanrewaju Buoro Emmanuella';
+  closing: string = 'Dedicated to ' + this.fullName;
+  closingLines: string[] = this.splitIntoLines(this.closing);
+
+  whiteColor: string = '#2495D9';
+  blackColor: string =  '#242626';
   redLightColor: string = '#BE101B'
   redDarkColor: string = '#860A24';
 
@@ -501,5 +507,4 @@ export class BirthdayGiftComponent {
       throw new Error('The total length of the array is not equal to 30.\n' + 'Total = ' + colorArray.length);
     }
   }
-
 }
